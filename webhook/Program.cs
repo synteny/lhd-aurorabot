9138674df4ad20522dc4248eaeb832a8d61a8bba
Vladimir Smirnov <synteny@users.noreply.github.com>
2019-12-07 14:44:42 +0300

    fix binding heroku port

--- webhook/Program.cs
+++ webhook/Program.cs
@@ -14,7 +14,10 @@ namespace Telegram.Bot.Examples.DotNetCoreWebHook
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls($"http://localhost:{Environment.GetEnvironmentVariable("PORT") ?? "5000"}")
+                .UseKestrel(options =>
+                {
+                    options.ListenLocalhost(Int32.Parse(System.Environment.GetEnvironmentVariable("PORT") ?? "5000"));
+                })
                 .Build();
     }
 }
